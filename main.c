ff0774f0598a43b8539c2a407486129b9895d21a
bluMATRIKZ <blumatrikz@gmail.com>
2025-06-22 19:46:58 -0700

    Add copyright info

--- main.c
+++ main.c
@@ -1,3 +1,21 @@
+/*
+png2xga - a PNG to my 4 bit color image format converter
+Copyright (C) 2025 Connor Thomson
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
